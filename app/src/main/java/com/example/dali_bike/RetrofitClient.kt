0a64243a823caaedad0081e6eb86c7be3e81f152
[chae] <[kchh0925@naver.com]>
2024-06-11 03:52:46 +0900

    sibal post

--- app/src/main/java/com/example/dali_bike/RetrofitClient.kt
+++ app/src/main/java/com/example/dali_bike/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.10:3000"
+    private const val BASE_URL = "http://192.168.45.242:3000"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
--- app/src/main/java/com/example/dali_bike/api/RetrofitClient.kt
+++ app/src/main/java/com/example/dali_bike/api/RetrofitClient.kt
@@ -22,7 +22,7 @@ import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 
-private const val BASE_URL = "http://192.168.0.10:3000"
+private const val BASE_URL = "http://192.168.45.242:3000"
 
 interface ApiInterface {
     @POST("/user/login")
